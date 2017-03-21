11a8ea6e3409232dfff6982a0c41f02eef286321
Andreas <amarkauskas1@gmail.com>
2017-03-21 20:59:03 +1100

    End of lesson 5

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Synopsis
 
-This is a project of completed work tasks for the Introduction to Javascript class @ Coder Factory.
+This is a project of completed work tasks and side notes for the Introduction to Javascript class @ Coder Factory.
+
+## Caveat
+
+This is not meant to be helpful for other people except Andreas.
--- lesson5.js
+++ lesson5.js
@@ -28,3 +28,30 @@ console.log(obj);
 
 // Or
 obj.test = "valuable";
+
+// indexOf and lastIndexOf
+// find out if an array contains two of the same number/letter
+// slice - first arg is point of start, second arg is last point in array
+
+var info = ['a', 'b', 'c', 'd', 'b', 'e'];
+
+for (var i = 0; i < info.length; i++) {
+    if (info.indexOf(info[i]) !== info.lastIndexOf(info[i])) {
+        console.log("Duplicate values found! " + info[i]);
+    }
+}
+
+// side problem, finding non duplicate array
+var info = ['a', 'b', 'c', 'd', 'b', 'e'];
+var out = [];
+var obj = {};
+
+console.log(info);
+
+for (var i = 0; i < info.length; i++) {
+    obj[info[i]] = 0;
+}
+for (var key in obj) {
+    out.push(key);
+}
+console.log(out);
